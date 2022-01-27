380ef1fdec6fb6f5d6da264617eb7d8db4596caa
M.Angel Juan <m.angel.juan@gmail.com>
2022-01-27 22:43:52 +0100

    disable network tests for baikonur building

---
+++ debian/tests/excludes/any/TestHttp.rb
@@ -0,0 +1,3 @@
+# Builder not allow network or socket usage
+exclude :test_bind_to_local_host, 'fails on baikonur builder'
+exclude :test_bind_to_local_port, 'fails on baikonur builder'
---
+++ debian/tests/excludes/any/TestSmtp.rb
@@ -0,0 +1,4 @@
+# Builder not allow network or socket usage
+exclude :test_eof_error_backtrace, 'fails on baikonur builder'
+exclude :test_tls_connect, 'fails on baikonur builder'
+exclude :test_tls_connect_timeout, 'fails on baikonur builder'
